a1229017bb972aa193acdc7f34db26a68ecc2b3f
Illia Skliarov <illia.skliarov@collibra.com>
2021-08-29 17:17:10 +0200

    auto increment for pk

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employees (
-    id              INTEGER NOT NULL PRIMARY KEY,
+    id              INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name      VARCHAR(30) NOT NULL,
     last_name       VARCHAR(30) NOT NULL,
     age             INTEGER NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE employee_addresses (
-    id              INTEGER NOT NULL PRIMARY KEY,
+    id              INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     employee_id     INTEGER NOT NULL REFERENCES employees(id),
     address         VARCHAR(128) NOT NULL
 );
